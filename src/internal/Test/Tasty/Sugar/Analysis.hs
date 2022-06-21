2a6a66f9b5cbecbf6520674a9e9993763047ecfd
Kevin Quick <kquick@galois.com>
2022-06-20 23:50:47 -0700

    Merge Sweets results by choosing the best expected file match (by explicit params)

--- src/internal/Test/Tasty/Sugar/Analysis.hs
+++ src/internal/Test/Tasty/Sugar/Analysis.hs
@@ -9,12 +9,16 @@ where
 
 import           Control.Monad.Logic
 import           Data.Bifunctor ( bimap )
+import           Data.Function ( on )
+import qualified Data.List as L
 import           Data.Maybe ( catMaybes )
+import           Data.Ord ( comparing )
 import qualified System.FilePath as FP
 import qualified System.FilePath.GlobPattern as FPGP
 
 import           Test.Tasty.Sugar.ExpectCheck
 import           Test.Tasty.Sugar.RootCheck
+import           Test.Tasty.Sugar.ParamCheck ( pmatchCmp )
 import           Test.Tasty.Sugar.Types
 
 
@@ -48,22 +52,54 @@ checkRoot pat allNames rootNm =
       params = validParams pat
       combineExpRes (swts, expl) = bimap (swts :) (expl :)
 
-      trimSweets :: [(Sweets, SweetExplanation)] ->
-                    [(Sweets, SweetExplanation)]
-      trimSweets =
-        -- If multiple Sweets have the same rootMatchName, use the one
-        -- with the longer rootBaseName.  This prevents "foo.exp" and
-        -- "foo-bar.exp" from both matching "foo-bar.c".
-        (\l -> let removeShorterBases lst (entry,_) =
-                     let notShorterBase (s,_) = not $
-                           and [ rootMatchName s == rootMatchName entry
-                               , rootBaseName s < rootBaseName entry
-                               ]
-                     in filter notShorterBase lst
-               in foldl removeShorterBases l l)
+      mergeSweets swl =
+        -- If multiple Sweets have the same rootMatchName this likely means that
+        -- there were multiple expected files that could have matched.  Merge the
+        -- Expectations: for each of the second Sweet's expectations:
+        --
+        --   - If one has a longer rootBaseName, that one represents the more
+        --     explicit match and should be used.  Otherwise,
+        --
+        --   - If no explicit (expParamsMatch) elements match the first, this
+        --     is a unique Expectation, add it to the first Sweet
+        --
+        --   - Find the Expectation in the first Sweet with the most number of
+        --     Explicit matches, then select the one that has the most number of
+        --     remaining Explicit that don't match the other (one should be a
+        --     strict subset of the other!)
+        let combineIfRootsMatch s sl =
+              -- Add s to sl, or if s matches a root in sl, merge s with that sl
+              uncurry (flip (:))
+              ( combineSweets s <$> L.partition (not . isRootMatch s) sl)
+            isRootMatch = (==) `on` (rootMatchName . fst)
+            combineSweets s slm =
+              -- Merge all the expectations from each of slm sweets into the main
+              -- sweet s.
+              foldr chooseOrCombineExpectations s slm
+            chooseOrCombineExpectations (s,e) (sm,sme) =
+              case comparing rootBaseName s sm of
+                GT -> (s,e)
+                LT -> (sm, sme)
+                EQ -> bestExpectations (s,e) (sm,sme)
+            bestExpectations (s,e) (sm,_sme) =
+              -- combine the expectations in s with the expectations in each of
+              -- sm, where expectations overlap based on explicit expParamsMatch
+              -- matchups.
+              let swts = s { expected =
+                               foldr mergeExp (expected s) (expected sm)
+                           }
+                  mergeExp oneExp exps =
+                    concat
+                    $ fmap (take 1)
+                    $ L.groupBy ((==) `on`
+                                  (fmap (fmap getParamVal) . expParamsMatch))
+                    $ L.sortBy (pmatchCmp `on` expParamsMatch)
+                    $ oneExp : exps
+              in ( swts, e { results = swts } )
+        in foldr combineIfRootsMatch [] swl
 
   in foldr combineExpRes ([], []) $
-     trimSweets $
+     mergeSweets $
      catMaybes $
      fmap (findExpectation pat rootNm allNames) $
      observeAll $
